c6599d1c8207798fb8d99101491a05fcacd5c4c5
LucArtOhm <90980974+LucArtOhm@users.noreply.github.com>
2021-09-26 22:55:32 +0200

    Write some Pokemon features
    State name, height, type from seven Pokemons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
-alert('Hello World');
-favoriteFood = 'Cereal';
-document.write(favoriteFood);
+var pokemonList = :[
+  {name: Lunala, height: 13\'01\", type: ['psychich', 'ghost']},
+  {name: Solgaleo, height: 11\'02\", type: ['psyhich', 'steel']},
+  {name: Psyduck, height: 2\'07\", type: 'water'},
+  {name: Yveltal, height: 19\', type: ['dark', 'flying']}
+  {name: Pilowswine, height: 3\'07\", type: ['ice', 'ground']}
+  {name: Unown, height: 1\'08\", type: 'psychich'}
+  {name: Magnemite, height: 1\', type: ['electric', 'steel']}
+];
