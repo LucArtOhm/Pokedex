a60d464b2161b03c3289fb5e4e06ea7c92b7061c
LucArtOhm <90980974+LucArtOhm@users.noreply.github.com>
2021-09-28 11:10:56 +0200

    Describe objects separately
    pokemonList variable array is created, assigned a blank array and then the separate objects detailed beschrieben.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,62 @@
-var pokemonList = :[
-  {name: Lunala, height: 13\'01\", type: ['psychich', 'ghost']},
-  {name: Solgaleo, height: 11\'02\", type: ['psyhich', 'steel']},
-  {name: Psyduck, height: 2\'07\", type: 'water'},
-  {name: Yveltal, height: 19\', type: ['dark', 'flying']}
-  {name: Pilowswine, height: 3\'07\", type: ['ice', 'ground']}
-  {name: Unown, height: 1\'08\", type: 'psychich'}
-  {name: Magnemite, height: 1\', type: ['electric', 'steel']}
-];
+/*
+* Create a new variable called pokemonList
+* and assign to it a blank array.
+*/
+
+let pokemonList = [];
+
+//Add several objects to the array
+
+let lunala = {
+    name: 'Lunala',
+    height: 13\'01\",
+    type: ['psychich', 'ghost']
+  };
+
+let solgaleo = {
+    name: 'Solgaleo',
+    height: 11\'02\",
+    type: ['psyhich', 'steel']
+  };
+
+let psyduck = {
+    name: 'Psyduck',
+    height: 2\'07\",
+    type: 'water'
+  };
+
+let yveltal = {
+    name: 'Yveltal',
+    height: 19\',
+    type: ['dark', 'flying']
+  };
+
+let pilowswine = {
+    name: 'Pilowswine',
+    height: 3\'07\",
+    type: ['ice', 'ground']
+  };
+
+let unown = {
+    name: 'Unown',
+    height: 1\'08\",
+    type: 'psychich'
+  };
+
+let magnemite = {
+    name: 'Magnemite',
+    height: 1\',
+    type: ['electric', 'steel']
+  };
+
+//Assigning of named objects to the blank pokemonList array
+
+pokemonList = [
+  lunala,
+  solgaleo,
+  psyduck,
+  yveltal,
+  pilowswine,
+  unown,
+  magnemite
+]
