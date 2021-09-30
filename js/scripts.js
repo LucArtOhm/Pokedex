87ab4896965b619eb0fbf3eb1093b3b8b44f547a
LucArtOhm <90980974+LucArtOhm@users.noreply.github.com>
2021-09-30 15:41:14 +0200

    Add for loop to find out biggest size Pokemon
    also condition to find out the smallest

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,49 @@
-//Add several objects to the array
-
-let lunala = {
-    name: 'Lunala',
-    height: 13,
-    type: ['psychich', 'ghost']
-  };
-
-let solgaleo = {
-    name: 'Solgaleo',
-    height: 11,
-    type: ['psyhich', 'steel']
-  };
-
-let psyduck = {
-    name: 'Psyduck',
-    height: 2,
-    type: 'water'
-  };
-
-let yveltal = {
-    name: 'Yveltal',
-    height: 19,
-    type: ['dark', 'flying']
-  };
-
-let pilowswine = {
-    name: 'Pilowswine',
-    height: 3,
-    type: ['ice', 'ground']
-  };
-
-let unown = {
-    name: 'Unown',
-    height: 1,
-    type: 'psychich'
-  };
-
-let magnemite = {
-    name: 'Magnemite',
-    height: 1,
-    type: ['electric', 'steel']
-  };
+let pokemonList = [
+  lunala = {
+      name: 'Lunala',
+      height: 13,
+      type: ['psychich', 'ghost']
+    },
+  solgaleo = {
+      name: 'Solgaleo',
+      height: 11,
+      type: ['psyhich', 'steel']
+    },
+  psyduck = {
+      name: 'Psyduck',
+      height: 2,
+      type: 'water'
+    },
+  yveltal = {
+      name: 'Yveltal',
+      height: 19,
+      type: ['dark', 'flying']
+    },
+  pilowswine = {
+      name: 'Pilowswine',
+      height: 3,
+      type: ['ice', 'ground']
+    },
+  unown = {
+      name: 'Unown',
+      height: 1,
+      type: 'psychich'
+    },
+  magnemite = {
+      name: 'Magnemite',
+      height: 1,
+      type: ['electric', 'steel']
+    },
+]
 
 //Here comes the 'for loop'
 
-let pokemonList = ['lunala (height: 13)', 'solgaleo (height: 11)', 'psyduck (height: 2)', 'yveltal (height: 19)', 'pilowswine (height: 3)', 'unown (height: 1)', 'magnemite (height: 1)'];
-let text = '';
-let i = 0
-
-for (;pokemonList [i];){
-  text = text + ' ' + pokemonList [i];
-  i++;
+for (let i = 0; i < pokemonList.length; i++){
+  if (pokemonList [i].height > 13 && pokemonList [i].height < 20){
+    document.write (pokemonList [i].name + ' (' + 'height: ' + pokemonList [i].height + ' )' + ' - Wow, she\'s enormous!!!');
+  } else if (pokemonList [i].height > 2){
+    document.write (pokemonList [i].name + ' (' + 'height: ' + pokemonList [i].height + ' )' + ' - a mini Poki!');
+  } else {
+    document.write (pokemonList [i].name + ' (' + 'height: ' + pokemonList [i].height + ' )');
+  }
 }
-document.write(text);
