47dd20313371224b0baf6a974cbfa35eafad809b
LucArtOhm <90980974+LucArtOhm@users.noreply.github.com>
2021-10-01 05:40:56 +0200

    Intend items more efficiently, erase the doubling of the names

--- js/scripts.js
+++ js/scripts.js
@@ -1,75 +1,75 @@
 let pokemonList = [
-  lunala = {
-      name: 'Lunala',
-      height: 13,
-      type: ['psychich', 'ghost'],
-      weakness: ['ghost', 'dark']
-    },
-  solgaleo = {
-      name: 'Solgaleo',
-      height: 11,
-      type: ['ghost', 'fire', 'dark', 'ground']
-    },
-  psyduck = {
-      name: 'Psyduck',
-      height: 2,
-      type: 'water',
-      weakness: ['grass', 'electric']
-    },
-  yveltal = {
-      name: 'Yveltal',
-      height: 19,
-      type: ['dark', 'flying'],
-      weakness: ['fairy', 'electric', 'ice', 'rock']
-    },
-  piloswine = {
-      name: 'Piloswine',
-      height: 3,
-      type: ['ice', 'ground'],
-      weakness: ['steel', 'fire', 'grass', 'water', 'fighting']
-    },
-  unown = {
-      name: 'Unown',
-      height: 1,
-      type: 'psychich',
-      weakness: ['ghost', 'dark', 'bug']
-    },
-  magnemite = {
-      name: 'Magnemite',
-      height: 1,
-      type: ['electric', 'steel'],
-      weakness: ['fire', 'fighting', 'ground']
-    },
-  mamoswine = {
-      name: 'Mamoswine',
-      height: 8,
-      type: ['ice', 'ground'],
-      weakness: ['steel', 'fire', 'grass', 'water', 'fighting']
-      },
-  eevee = {
-      name: 'Eevee',
-      height: 1,
-      type: ['normal'],
-      weakness: ['fighting']
-      },
-  pikachu = {
-      name: 'Pikachu',
-      height: 1,
-      type: ['electric'],
-      weakness: ['ground']
-      },
-  squirtle = {
-      name: 'Squirtle',
-      height: 2,
-      type: ['water'],
-      weakness: ['grass', 'electric']
-      },
-  poliwag = {
-      name: 'Poliwag',
-      height: 2,
-      type: ['water'],
-      weakness: ['grass', 'electric']
-      },
+  {
+    name: 'Lunala',
+    height: 13,
+    type: ['psychich', 'ghost'],
+    weakness: ['ghost', 'dark']
+  },
+  {
+    name: 'Solgaleo',
+    height: 11,
+    type: ['ghost', 'fire', 'dark', 'ground']
+  },
+  {
+    name: 'Psyduck',
+    height: 2,
+    type: 'water',
+    weakness: ['grass', 'electric']
+  },
+  {
+    name: 'Yveltal',
+    height: 19,
+    type: ['dark', 'flying'],
+    weakness: ['fairy', 'electric', 'ice', 'rock']
+  },
+  {
+    name: 'Piloswine',
+    height: 3,
+    type: ['ice', 'ground'],
+    weakness: ['steel', 'fire', 'grass', 'water', 'fighting']
+  },
+  {
+    name: 'Unown',
+    height: 1,
+    type: 'psychich',
+    weakness: ['ghost', 'dark', 'bug']
+  },
+  {
+    name: 'Magnemite',
+    height: 1,
+    type: ['electric', 'steel'],
+    weakness: ['fire', 'fighting', 'ground']
+  },
+  {
+    name: 'Mamoswine',
+    height: 8,
+    type: ['ice', 'ground'],
+    weakness: ['steel', 'fire', 'grass', 'water', 'fighting']
+  },
+  {
+    name: 'Eevee',
+    height: 1,
+    type: ['normal'],
+    weakness: ['fighting']
+  },
+  {
+    name: 'Pikachu',
+    height: 1,
+    type: ['electric'],
+    weakness: ['ground']
+  },
+  {
+    name: 'Squirtle',
+    height: 2,
+    type: ['water'],
+    weakness: ['grass', 'electric']
+  },
+  {
+    name: 'Poliwag',
+    height: 2,
+    type: ['water'],
+    weakness: ['grass', 'electric']
+  },
 ]
 
 //Here comes the 'for loop'
