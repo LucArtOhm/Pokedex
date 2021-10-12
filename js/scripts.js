865df4fd9b70a28cf0b347165aa30f9c98b19154
LucArtOhm <90980974+LucArtOhm@users.noreply.github.com>
2021-10-12 15:08:26 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ let pokemonRepository = (function() {
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
+  //Sort the Pokemon alphabetically
+  // const pokemonList = [];
+  // pokemonList.sort(up);
+
   function add(pokemon) {
     if (
       typeof pokemon === 'object' &&
