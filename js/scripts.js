7716932eda99d288878e9d87b68509fda4dee20b
LucArtOhm <90980974+LucArtOhm@users.noreply.github.com>
2021-10-15 22:18:47 +0200

    Add working hideModal function

--- js/scripts.js
+++ js/scripts.js
@@ -80,52 +80,71 @@ let pokemonRepository = (function() {
   }
 
 // //   //function to show the modal
-  function showModal() {
+  function showModal(title, text) {
     let modalContainer = document.querySelector('#modal-container');
+//     modalContainer.classList.add('is-visible');
+  // }
+
+// // //   function showModal(title, text) {
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+//
+    //to close the modal with the close button
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+
     modalContainer.classList.add('is-visible');
-  }
+}
 
   document.querySelector('#show-modal').addEventListener('click', () => {
-    showModal();
+    showModal('Modal Title', 'This is the modal content!');
   });
-// //
-// //
-// //   function showModal(title, text) {
-// //     modalContainer.innerHTML = '';
-// //
-// //     let modal = document.createElement('div');
-// //     modal.classList.add('modal');
-// //
-// //     let closeButtonElement = document.createElement('button');
-// //     closeButtonElement.classList.add('modal-close');
-// //     closeButtonElement.innerText = 'Close';
-// //     closeButtonElement.addEventListener('click', hideModal);
-// //
-// //     let titleElement = document.createElement('h1');
-// //     titleElement.innerText = title;
-// //
-// //     let contentElement = document.createElement('p');
-// //     contentElement.innerText = text;
-// //
-// //     modal.appendChild(closeButtonElement);
-// //     modal.appendChild(titleElement);
-// //     modal.appendChild(contentElement);
-// //     modalContainer.appendChild(modal);
-// //
-// //     modalContainer.classList.add('is-visible');
-// // }
 // //
 // // //In order to close the modal...
 // //   let dialogPromiseReject;
 // //
-// //   function hideModal() {
-// //     modalContainer.classList.remove('is-visible');
+  function hideModal() {
+    let modalContainer = document.querySelector('#modal-container');
+    modalContainer.classList.remove('is-visible');
 // //
+  //to close the modal with the Escape key
+  window.addEventListener('keydown', (e) => {
+    let modalContainer = document.querySelector('#modal-container');
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  //to close the modal by clicking outside the modal
+  modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal
+    //We want to close if the user clicks directlz on the overlay
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+  }
 // //     if (dialogPromiseReject) {
 // //       dialogPromiseReject();
 // //       dialogPromiseReject = null;
 // //     }
-// // }
+
 // //
 // //   function showDialog(title, text) {
 // //     showModal(title, text);
@@ -170,20 +189,8 @@ let pokemonRepository = (function() {
 // //     });
 // //   });
 // //
-// //   window.addEventListener('keydown', (e) => {
-// //     if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-// //       hideModal();
-// //     }
-// //   });
 // //
-// // modalContainer.addEventListener('click', (e) => {
-// //     // Since this is also triggered when clicking INSIDE the modal
-// //     //We want to close if the user clicks directlz on the overlay
-// //   let target = e.target;
-// //   if (target === modalContainer) {
-// //     hideModal();
-// //   }
-// // });
+
 // //
 // // document.querySelector('#show-modal').addEventListener ('click', () => {
 // //   showModal('Modal Title', 'This is the modal content, what?');
@@ -196,8 +203,8 @@ let pokemonRepository = (function() {
     addListItem: addListItem,
     loadList: loadList,
     loadDetails: loadDetails,
-    // showDetails: showDetails,
-    // showModal: showModal
+    showDetails: showDetails,
+    showModal: showModal
   };
 
 })();
