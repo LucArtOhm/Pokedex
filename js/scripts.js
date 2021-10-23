0c8419a2ebf5792422b73087d77a1e63ec724d05
LucArtOhm <90980974+LucArtOhm@users.noreply.github.com>
2021-10-23 13:21:30 +0200

    Revert "Fix Bootstrap code errors"
    This reverts commit d5643e97eb702e8c46d8fb4e1f83fc21751e505c.

--- js/scripts.js
+++ js/scripts.js
@@ -97,67 +97,39 @@ let pokemonRepository = (function() {
 
   // function to show the modal
   function showModal(pokemon) {
-    let modalBody = $('.modal-body');
-    let modalTitle = $('.modal-title');
-    let modalHeader = $('.modal-header');
 
-    modalTitle.empty();
-    modalBody.empty();
+    modalContainer.innerHTML = '';
 
-    // Create element for name in modal content
-    let nameElement = $('<h1>' + item.name + '</h1>');
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
 
-    //create image in modal content
-    let imageElementFront = $('<img class='modal-img' style='width: 50%'>');
-    imageElementFront.attr('src', item.imageUrlFront);
-    let imageElementBack = $('<img class='modal-img' style='width: 50%'>');
-    imageElementBack.attr('src', item.imageUrlBack);
+    //to close the modal with the close button
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'X';
+    closeButtonElement.addEventListener('click', hideModal);
 
-    //create element for height in modal content
-    let heightElement = $('<p>' + 'height: ' + item.height + '</p>');
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = pokemon.name;
 
-    //create element for weight in modal content
-    let weightElement = $('<p>' + 'weight: ' + item.weight + '</p>');
+    let heightElement = document.createElement('p');
+    heightElement.classList.add('pokemon-height');
+    heightElement.innerText = 'Height: ' + pokemon.height;
 
-    //create element for types in modal content
-    let typesElement = $('<p>' + 'types: ' + item.types + '</p>');
+    let imageElement = document.createElement('img');
+    imageElement.classList.add('pokemon-img');
+    imageElement.src = pokemon.imageUrl;
 
 
-    // let modal = document.createElement('div');
-    // modal.classList.add('modal');
-    //
-    // //to close the modal with the close button
-    // let closeButtonElement = document.createElement('button');
-    // closeButtonElement.classList.add('modal-close');
-    // closeButtonElement.innerText = 'X';
-    // closeButtonElement.addEventListener('click', hideModal);
-    //
-    // let titleElement = document.createElement('h1');
-    // titleElement.innerText = pokemon.name;
-    //
-    // let heightElement = document.createElement('p');
-    // heightElement.classList.add('pokemon-height');
-    // heightElement.innerText = 'Height: ' + pokemon.height;
-    //
-    // let imageElement = document.createElement('img');
-    // imageElement.classList.add('pokemon-img');
-    // imageElement.src = pokemon.imageUrl;
 
-    modalTitle.append(nameElement);
-    modalBody.append(imageUrlFront);
-    modalBody.append(imageUrlBack);
-    modalBody.append(heightElement);
-    modalBody.append(weightElement);
-    modalBody.append(typesElement);
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(heightElement);
+    modal.appendChild(imageElement);
+    modalContainer.appendChild(modal);
 
-//     modal.appendChild(closeButtonElement);
-//     modal.appendChild(titleElement);
-//     modal.appendChild(heightElement);
-//     modal.appendChild(imageElement);
-//     modalContainer.appendChild(modal);
-//
-//     modalContainer.classList.add('is-visible');
-// }
+    modalContainer.classList.add('is-visible');
+}
 
   // document.querySelector('#show-modal').addEventListener('click', () => {
   //   showModal('Modal Title', 'This is the modal content!');
